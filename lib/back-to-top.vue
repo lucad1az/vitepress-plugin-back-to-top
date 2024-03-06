e816567b2df9dc5d1019ae3ca0b47966e0f254d2
lucad1az <99685103+lucad1az@users.noreply.github.com>
2024-03-06 14:40:53 -0300

    changes svg stroke to fix it's visibility in dark mode

--- lib/back-to-top.vue
+++ lib/back-to-top.vue
@@ -6,7 +6,7 @@
         viewBox="0 0 48 48"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        stroke="currentColor"
+        stroke="black"
         stroke-width="4"
         stroke-linecap="butt"
         stroke-linejoin="miter"
